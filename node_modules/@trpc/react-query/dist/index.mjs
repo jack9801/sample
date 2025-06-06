b808a44b8599532a4f22eb6e602e269b1aff6973
jack9801 <shishupaljnvr@gmail.com>
2025-06-07 00:12:44 +0530

    first commit

--- node_modules/@trpc/react-query/dist/index.mjs
+++
@@ -1,4 +0,0 @@
-export * from '@trpc/client';
-export { getMutationKey, getQueryKey } from './internals/getQueryKey.mjs';
-export { createTRPCReact } from './createTRPCReact.mjs';
-export { createTRPCQueryUtils } from './createTRPCQueryUtils.mjs';
